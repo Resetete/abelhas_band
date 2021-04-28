6a9aff2c6c9a3e02d7792d849f92ca6bc5207067
Theresa <tmannschatz@gmail.com>
2021-04-28 22:07:38 +0200

    Add a thank you message div to the page when submitting of the form was successfull

--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxtmW7XKfiNlHuQl1Oh5wmmtABrI8yj17mm3cEdLcfnXQpaiH7I-KZbN65yIBIUdh0/exec'
 const form = document.forms['submit-to-google-sheet']
 
+
 form.addEventListener('submit', e => {
   e.preventDefault()
+
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert('Thank you for your email!', response))
-    .catch(error => alert('Sorry, something went wrong. Please try again', error.message))
+    .then(response => { display_thankyou_message(response) })
+    .catch(error => console.log('Sorry, something went wrong. Please try again', error.message))
 })
+
+function display_thankyou_message(response) {
+  var test = $('.thankyou_message').css('display', 'block');
+  console.log(response);
+};
